c6e761c55c58be0f2ef9c7e9daf0790aabe17d54
Ntini <95422134+Simontine@users.noreply.github.com>
2024-05-08 23:54:25 +0200

    Blogs Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { LandingPageComponent } from "./components/landing-page/landing-page.com
 import { ServicesComponent } from "./components/services/services.component";
 import { SearchComponent } from "./components/search/search.component";
 import { NavComponent } from './components/nav/nav.component';
+import { BlogsComponent } from "./components/blogs/blogs.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavComponent, ServicesComponent, SearchComponent]
+    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavComponent, ServicesComponent, SearchComponent, BlogsComponent]
 })
 export class AppComponent {
   title = 'Ampl Shop';
