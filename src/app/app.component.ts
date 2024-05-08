914f94bf49ef2dfea02b1f1d770cdc2efb472bf2
Ntini <95422134+Simontine@users.noreply.github.com>
2024-05-08 23:59:59 +0200

    Footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import { ServicesComponent } from "./components/services/services.component";
 import { SearchComponent } from "./components/search/search.component";
 import { NavComponent } from './components/nav/nav.component';
 import { BlogsComponent } from "./components/blogs/blogs.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavComponent, ServicesComponent, SearchComponent, BlogsComponent]
+    imports: [CommonModule, RouterOutlet, LandingPageComponent, NavComponent, ServicesComponent, SearchComponent, BlogsComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'Ampl Shop';
